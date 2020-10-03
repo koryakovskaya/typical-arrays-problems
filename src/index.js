f84d59695a0e87303bd239f63560a14791b7bc3a
Userkoryakovskaya <koryakovskaya.tatiana@gmail.com>
2020-10-03 22:52:26 +0300

    fix: fix solutions

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 //min
 
 exports.min = function min (array) {      
-  let minValueInArray = array.length > 0 ? Math.min.apply(null, array) : 0;
-  console.log (minValueInArray);
+  if (!Array.isArray(array) || array.length == 0) return 0;
+	return Math.min.apply(null, array);
   }
   
 
 //max  
 
   exports.max = function max (array) {      
-  let maxValueInArray = array.length > 0 ? Math.max.apply(null, array) : 0;
-  return maxValueInArray;
+    if (!Array.isArray(array) || array.length == 0) return 0;
+    return Math.max.apply(null, array);
   }
   
   
