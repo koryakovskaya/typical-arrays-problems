fdac27f70f75b3131c8edeab6d6d7168a6700d2c
Userkoryakovskaya <koryakovskaya.tatiana@gmail.com>
2020-10-03 13:44:09 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+//min
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = function min (array) {      
+  let minValueInArray = array.length > 0 ? Math.min.apply(null, array) : 0;
+  console.log (minValueInArray);
+  }
+  
 
-exports.avg = function avg (array) {
-  return 0;
-}
+//max  
+
+  exports.max = function max (array) {      
+  let maxValueInArray = array.length > 0 ? Math.max.apply(null, array) : 0;
+  return maxValueInArray;
+  }
+  
+  
+//avg
+
+  exports.avg = function avg (array) {    
+    if(array.length > 0) {
+      return array.reduce((sum, cur) => sum + cur) / array.length;
+      }  
+        return 0;
+    }
+  
